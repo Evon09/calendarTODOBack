e9fb989db64378906a40d9cdb0c60ab6415ac3c6
Guilherme Shibuya <gyudishibuya@gmail.com>
2026-05-17 13:47:17 -0300

    add class validator

--- calendar-todo/src/main.ts
+++ calendar-todo/src/main.ts
@@ -1,8 +1,19 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3333, '0.0.0.0').then(() => {
     console.log(`Server is running on port ${process.env.PORT ?? 3333}`);
   });
--- calendar-todo/src/modules/users/dto/signin-user.dto.ts
+++ calendar-todo/src/modules/users/dto/signin-user.dto.ts
@@ -1,4 +1,10 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class SignInUserDto {
-  email: string;
-  password: string;
+  @IsEmail()
+  email!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password!: string;
 }
